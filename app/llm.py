72cf92937bd4a799566d8b57f322029aa24e41bb
Loki-33 <arshadr2079@gmail.com>
2026-01-08 05:23:38 +0530

    API KEY

--- app/llm.py
+++ app/llm.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+if not OPENROUTER_API_KEY:
+    print("ERROR: OPENROUTER_API_KEY not found!!!")
+else:
+    print("API KEY LOADED SUCCESSFULLY")
 URL = "https://openrouter.ai/api/v1/chat/completions"
 
 HEADERS = {
